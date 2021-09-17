9c7d30fc643ed1671c4ec0a7fa2b402045580a2a
Jake Kazi <kazijake@gmail.com>
2021-09-16 20:17:21 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+  {
+    'quote': 'It is only with the heart that one can see rightly; what is essential is invisible to the eye.',
+    'source': 'The Fox from The Little Prince'
+  },
+  {
+    'quote': 'The horizon of many people is a circle with zero radius which they call their point of view.',
+    'source': 'Albert Einstein'
+  },
+  {
+    'quote': 'And, when you want something, all the universe conspires in helping you to achieve it.',
+    'source': 'Paulo Coelho'
+  },
+  {
+    'quote': 'We accept the love we think we deserve',
+    'source': 'Stephen Chbosky'
+  },
+  {
+    'quote': 'Life is either a daring adventure or nothing.',
+    'source': 'Helen Keller'
+  },
+]
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
